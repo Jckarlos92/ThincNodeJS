ed79d3a2be0ff57968bbe34132288749e5315cbf
Juan Carlos <jckarlos92@hotmail.com>
2015-12-06 18:57:20 -0600

    Functional Chat. Beta Version

--- public/app/messages/controllers.js
+++ public/app/messages/controllers.js
@@ -1,6 +1,22 @@
 var chatControllers = angular.module('chatControllers', []);
+var socket = io();
 
-chatControllers.controller('ChatCtrl', ['$scope', 
+chatControllers.controller('ChatCtrl', ['$scope',
   function ($scope) {
+    var max = 101;
+    var min = 1;
+    $scope.username = 'user_' + (Math.floor(Math.random() * (max - min)) + 1);
+    $scope.messages = [];
 
-}]);
+    socket.on('newMessage', function (msg) {
+      $scope.messages.push(msg);
+      $scope.$apply(); // Refres the view
+    });
+
+      $scope.sendMessage = function (msg) {
+        socket.emit('newMessage', {
+          user: $scope.username,
+          text: msg 
+        }); 
+      };
+  }]);
