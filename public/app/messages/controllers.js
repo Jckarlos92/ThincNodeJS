19a8109f8d18e176d0df2605349eece4d7ebc6ed
Juan Carlos <jckarlos92@hotmail.com>
2015-12-07 21:14:38 -0600

    Chat cambiado a firebase

--- public/app/messages/controllers.js
+++ public/app/messages/controllers.js
@@ -1,22 +1,30 @@
 var chatControllers = angular.module('chatControllers', []);
 var socket = io();
 
-chatControllers.controller('ChatCtrl', ['$scope',
-  function ($scope) {
+chatControllers.controller('ChatCtrl', ['$scope', '$http',
+  function ($scope, $http) {
     var max = 101;
     var min = 1;
     $scope.username = 'user_' + (Math.floor(Math.random() * (max - min)) + 1);
     $scope.messages = [];
+
+    $http.get('/api/messages').success(function(data){
+      $scope.messages = data;
+    });
 
     socket.on('newMessage', function (msg) {
       $scope.messages.push(msg);
       $scope.$apply(); // Refres the view
     });
 
-      $scope.sendMessage = function (msg) {
-        socket.emit('newMessage', {
+    $scope.sendMessage = function (msg) {
+        var message = {
           user: $scope.username,
-          text: msg 
-        }); 
+          text: msg
+        }
+
+        $http.post('/api/messages', message);
+
+        socket.emit('newMessage', message); 
       };
   }]);
